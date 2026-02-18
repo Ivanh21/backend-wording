385868b49d410e7c25ded5869f497cca9d767a3b
Ivanh21 <ivanhtati07@gmail.com>
2026-02-18 18:40:20 +0000

    First commit

---
+++ src/main/java/com/example/backendwording/BackendWordingApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendwording;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendWordingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendWordingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backendwording/BackendWordingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendwording;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendWordingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
